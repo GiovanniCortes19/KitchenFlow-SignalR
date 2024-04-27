4f70b95294d3d892d8dd9dd6fdb422b316d5c49a
GiovanniCortes <giocortes19@gmail.com>
2024-04-27 18:36:52 -0400

    Add DataContext and SeedingWorker classes

---
+++ KitchenFlow/Contexts/DataContext.cs
@@ -0,0 +1,17 @@
+﻿// Entity Framework
+using Microsoft.EntityFrameworkCore;
+using KitchenFlow.Models;
+
+namespace KitchenFlow.Contexts;
+
+public class DataContext : DbContext
+{
+  public DataContext(DbContextOptions<DataContext> options) : base(options)
+  {
+
+  }
+
+  public DbSet<FoodItem> FoodItems { get; set; }
+  public DbSet<Order> Orders { get; set; }
+
+}
