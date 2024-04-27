5239168f19c13c8bf8b75ef8be8bde247d9af99f
GiovanniCortes <giocortes19@gmail.com>
2024-04-27 14:29:35 -0400

    Add FoodItem and Order models

---
+++ KitchenFlow/Models/Order.cs
@@ -0,0 +1,22 @@
+﻿using System.Text.Json.Serialization;
+
+namespace KitchenFlow.Models;
+
+public class Order
+{
+  public int ID { get; set; }
+  public int TableNumber { get; set; }
+  public int FoodItemID { get; set; }
+  public FoodItem FoodItem { get; set; }
+  public DateTimeOffset OrderDate { get; set; }
+  public OrderStatus OrderStatus { get; set; }
+}
+
+[JsonConverter(typeof(JsonStringEnumConverter))]
+public enum OrderStatus
+{
+  Ordered,
+  Preparing,
+  AwaitingDelivery,
+  Delivered
+}
