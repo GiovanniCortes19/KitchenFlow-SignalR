28218e7da4df643589b88c1d715b0bd4eb9f26dc
GiovanniCortes <giocortes19@gmail.com>
2024-05-03 19:42:29 -0400

    Add route for KitchenComponent in app.routes.ts

--- UI-KitchenFlow/src/app/app.routes.ts
+++ UI-KitchenFlow/src/app/app.routes.ts
@@ -1,3 +1,8 @@
 import { Routes } from '@angular/router';
+import { CustomersComponent } from './Pages/customers/customers.component';
+import { KitchenComponent } from './Pages/kitchen/kitchen.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: 'customers', component: CustomersComponent },
+  { path: 'kitchen', component: KitchenComponent },
+];
